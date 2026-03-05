6784b5c43cf173db7913f2c9239b9bbcf39177df
kike <jerb5665@gmail.com>
2026-03-05 19:38:03 -0400

    Creation of template and static directories, plus a fix in settings to configure these directories

--- z_config/settings.py
+++ z_config/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'z_config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
