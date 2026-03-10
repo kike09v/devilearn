870e88ecd3049be0476181382aec6e395fcb7853
kike <jerb5665@gmail.com>
2026-03-10 14:44:56 -0400

    Fixing the base template and creating the routes for the course app

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def course_list(request):
+    return HttpResponse("<h1>Hola Mundo desde la lista de cursos</h1>")
+
+def course_detail(request):
+    return HttpResponse("<h1>Hola Mundo desde el detalle del curso</h1>")
+
+def course_lessons(request):
+    return HttpResponse("<h1>Hola Mundo desde las lecciones del curso</h1>")
